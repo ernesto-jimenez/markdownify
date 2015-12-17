854903d3e37296db98ebb012e67260e0eb892f4c
Ernesto Jiménez <me@ernesto-jimenez.com>
2015-12-17 13:48:33 +0100

    Lists support

--- markdownify.go
+++ markdownify.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"regexp"
+	"strconv"
 	"strings"
 	"unicode"
 
@@ -27,25 +28,25 @@ func markdownify(s *goquery.Selection) string {
 
 	// Slightly optimized vs calling Each: no single selection object created
 	for _, n := range s.Nodes {
-		buf.WriteString(getNodeText(n))
+		buf.WriteString(getNodeText(n, ""))
 	}
 	return tooManyNLRetexp.ReplaceAllString(strings.TrimFunc(buf.String(), unicode.IsSpace), "\n\n")
 }
 
 // Get the specified node's text content.
 // BUG: It doesn't respect <pre> tags
-func getNodeText(node *html.Node) string {
+func getNodeText(node *html.Node, indentation string) string {
 	var buf bytes.Buffer
 	// Clear redundant whitespace from text
 	if node.Type == html.TextNode {
 		text := normalizeWhitespace(node.Data)
-		if node.NextSibling == nil || isBlock(node.NextSibling) {
+		if node.NextSibling == nil || isBlock(node.NextSibling) || node.NextSibling.Data == "li" {
 			text = strings.TrimRightFunc(text, unicode.IsSpace)
 		}
 		if isBlock(node.NextSibling) {
 			text = text + "\n\n"
 		}
-		if isBlock(node.PrevSibling) {
+		if isBlock(node.PrevSibling) || (node.PrevSibling != nil && node.PrevSibling.Data == "li") {
 			text = strings.TrimLeftFunc(text, unicode.IsSpace)
 		}
 		return text
@@ -59,7 +60,7 @@ func getNodeText(node *html.Node) string {
 	}
 	if node.Data == "a" {
 		href, exists := getAttributeValue("href", node)
-		text := getNodeText(node.FirstChild)
+		text := getNodeText(node.FirstChild, indentation)
 		if !exists {
 			return text
 		}
@@ -70,10 +71,11 @@ func getNodeText(node *html.Node) string {
 	}
 	//buf.WriteString("=> " + node.Data + "|")
 	if isHeader(node) {
-		buf.WriteString("# ")
+		buf.WriteString(headerLevel(node))
+		buf.WriteString(" ")
 	}
 	for c := node.FirstChild; c != nil; c = c.NextSibling {
-		buf.WriteString(getNodeText(c))
+		buf.WriteString(getNodeText(c, indentation))
 	}
 	if isQuote(node) {
 		str := strings.TrimSpace(buf.String())
@@ -91,11 +93,29 @@ func getNodeText(node *html.Node) string {
 	if isBlock(node) {
 		buf.WriteString("\n\n")
 	}
-	return buf.String()
+	if node.Data == "li" {
+		txt := "* " + strings.TrimFunc(indentText(buf.String(), "  "), unicode.IsSpace)
+		return txt + "\n"
+	}
+	if node.Data == "ul" {
+		return indentText(strings.TrimFunc(buf.String(), unicode.IsSpace), indentation)
+	}
+	return indentText(buf.String(), indentation)
 }
 
 func isBlock(node *html.Node) bool {
 	return node != nil && (node.Data == "div" || isParagraph(node) || isHeader(node) || isQuote(node) || isList(node))
+}
+
+func indentText(str, indentation string) string {
+	lines := strings.Split(str, "\n")
+	for i, line := range lines {
+		lines[i] = indentation + line
+		if strings.TrimSpace(lines[i]) == "" {
+			lines[i] = ""
+		}
+	}
+	return strings.Join(lines, "\n")
 }
 
 func isQuote(node *html.Node) bool {
@@ -112,6 +132,17 @@ func isParagraph(node *html.Node) bool {
 
 func isHeader(node *html.Node) bool {
 	return node != nil && len(node.Data) == 2 && node.Data[0] == 'h' && node.Data[1] != 'r'
+}
+
+func headerLevel(node *html.Node) string {
+	if !isHeader(node) {
+		return ""
+	}
+	str := ""
+	for i, _ := strconv.Atoi(node.Data[1:]); i > 0; i-- {
+		str += "#"
+	}
+	return str
 }
 
 // Private function to get the specified attribute's value from a node.
--- testdata/markdown.md
+++ testdata/markdown.md
@@ -8,17 +8,31 @@ Paragraph separated.
 
 Two brs found.
 
-# Second section
+## Second section
 
 This part contains[ two](link) links [here](link2)
 
-# This title was in the same line
+## This title was in the same line
 
 as this paragraph
 
 > To quote or not to quote
 > Second quote line
 
+between quotes
+
 > Quote paragraph 1
 >
 > Quote paragraph 2
+
+* List item 1
+* List item 2
+* List item 3
+
+  * LI 3.1
+  * LI 3.2
+  * LI 3.3 paragraph 1
+
+    LI 3.3 paragraph 2
+* List item 4
+* ##### Header list item 5
